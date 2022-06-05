d578fffe58ff8a27dbf02784c83acdb7cb70c6a0
josevargas0310 <jvargash1@miumg.edu.gt>
2022-06-05 14:36:57 -0600

    se completa la interfaz del programa como su menu y metodos ya finalizados

--- RegistroDeDepartamentos/src/registrodedepartamentos/RegistroDeDepartamentos.java
+++ RegistroDeDepartamentos/src/registrodedepartamentos/RegistroDeDepartamentos.java
@@ -37,4 +37,82 @@ listas obj = new listas();
                       objeto.municipios();
                        System.out.println("si deseas seguir registrando departaemntos ingresa 0, de lo contrario ingresa 1.");
                       seguir =scanner.nextInt();
-              
+              }while(seguir==0);
+                  break;
+                  case 2:
+                      objeto.mostrarDepas();
+                   break;
+                  case 3:
+                      objeto.verificador();
+                      
+                   break;
+                  case 4: 
+                      objeto.vaciarDepas();
+                      objeto.vaciarCabeceras();
+                      objeto.vaciarMunicipios();
+                   break;
+                  case 5:
+                      objeto.mostrarDepas();
+                      objeto.mostrarCabeceras();
+                      objeto.mostrarMunicipios();
+              }
+              System.out.println("si deseas continuar ingresa 0, de lo contrario ingrese 6");
+              ok=scanner.nextInt();
+              }while(ok==0);
+    
+            
+        }if(opcion==2){
+            int seguir;
+            metodos utilizable= new metodos();
+            do{ 
+            System.out.println("/-----------------------------------------------------------------------------------------------------/\n"
+                + "/                           BIENVENIDO AL SISTEMA DE REGISTRO DE DEPARTAMENTOS                        /\n"
+                + "/-----------------------------------------------------------------------------------------------------/\n"
+                + "/  A continuacion se le presenta distintas opciones escoja la opcion deseada                          /\n"
+                + "/                                                                                                     /\n"
+                + "/  1. REGISTRAR DEPARTAMENTOS              2. BUSCAR UN DEPARTAMENTO REGISTRADO                       /\n"
+                + "/  3. MOSTRAR CABECERAS DEPARTAMENTALES    4. MOSTRAR municipios                                      /\n"
+                + "/  5. MOSTRAR TODOS LOS DATOS REGISTRADOS                                                             /\n"
+                + "/-----------------------------------------------------------------------------------------------------/\n");
+            int opc=scanner.nextInt();
+            switch(opc){
+                case 1: 
+                    int auxiliar;
+                    do{
+                    utilizable.departamentos();
+                    utilizable.cabeceras();
+                    utilizable.municipios();
+                    System.out.println("si desea seguir registrando departamentos ingrese 0, de lo contrario ingrese 6");
+                    auxiliar=scanner.nextInt();
+                    }while(auxiliar==0);
+                break;
+                case 2:
+                    utilizable.busquedaDapa();
+                break;
+                case 3:
+                    utilizable.mostrarCabeceras();
+                  break;
+                case 4:
+                    utilizable.mostrarMunicipios();
+                    break;
+                case 5: 
+                    utilizable.mostrarDepartamentos();
+                    utilizable.mostrarMunicipios();
+                    utilizable.mostrarCabeceras();
+                break;
+                    
+                    
+            }
+            System.out.println("si deseas continuar ingresa 0");
+            seguir=scanner.nextInt();
+            }while(seguir==0);
+            
+            
+        }else{
+            System.out.println("Por favor ingrese uno de los dos digitos que se le presentan");
+        }
+    
+    
+
+}
+}
