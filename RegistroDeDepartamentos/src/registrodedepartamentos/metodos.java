290b3006382a794008d6c9d321bf409989754ca7
josevargas0310 <jvargash1@miumg.edu.gt>
2022-06-05 14:28:38 -0600

    se hacen metodos para registrasr los datos del usuario y se agregan de nuevo al programa, ya que ya se habian hecho pero no se habia hecho los commits mientras se iban cumpliendo los metodos

--- RegistroDeDepartamentos/src/registrodedepartamentos/metodos.java
+++ RegistroDeDepartamentos/src/registrodedepartamentos/metodos.java
@@ -1,2 +1,32 @@
 
+package registrodedepartamentos;
+import java.util.Scanner;
+public class metodos {
+    static Scanner scanner = new Scanner (System.in);
+    private String[] departamentos;
+    private String[]cabeceras;
+    private String[]municipios;
+    private int inicio;
+    
+    public metodos(){
+    cabeceras=new String[24];
+    municipios=new String[100];
+    departamentos = new String[24];
+    }
+    
+    //metodos
+    public void departamentos(){
+    System.out.println("Ingrese los nombres de los departamentos");
+    String departamento=scanner.next();
+    inicio=inicio+1;
+    departamentos[inicio]="Departamento>>>> "+departamento;
+    
+    }
+    public void municipios (){
+    System.out.println("Ingrese la cantidad de municipios del departamento");
+    String muni=scanner.next();
+    inicio=inicio+1;
+    municipios[inicio]= "Cantidad de municipios>>>>"+muni;
+    }
+
 
